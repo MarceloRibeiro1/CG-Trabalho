588df220a3b45a076bf434f1a3f8b4a3b9b67522
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-01-03 19:00:05 -0300

    Trabalho

---
+++ works/enviroment.js
@@ -0,0 +1,381 @@
+import * as THREE from '../../build/three.module.js';
+import {TrackballControls} from '../../build/jsm/controls/TrackballControls.js';
+
+export class Block{
+    
+    constructor(x, y, z, initial){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.initial = initial;
+        this.passedBy = false;
+        this.blockSize = 20;
+
+        var cubeGeometry = new THREE.BoxGeometry(this.blockSize*0.98, 0.3, this.blockSize*0.98);
+        var cubeGeometry2 = new THREE.BoxGeometry(this.blockSize, 0.2, this.blockSize);
+        var cubeMaterial2 = new THREE.MeshPhongMaterial({color: "rgba(255, 0, 0)", side: THREE.DoubleSide,});
+        if(initial){
+            var cubeMaterial = new THREE.MeshPhongMaterial({color: "rgba(255, 126, 0)", side: THREE.DoubleSide,});
+        }else{
+            var cubeMaterial = new THREE.MeshPhongMaterial({color: "rgba(235, 235, 220)", side: THREE.DoubleSide,});
+        }        
+        this.cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        this.cube.position.set(x, y, z);
+
+        this.cubeFundo = new THREE.Mesh(cubeGeometry2, cubeMaterial2);
+        this.cubeFundo.position.set(x, y, z);
+        //scene.add(cube)
+    }
+
+    get block() {
+        return this.cube;
+    }
+    get fundo(){
+        return this.cubeFundo;
+    }
+}
+
+export class Speedway{
+    constructor(sideSize, type){
+        this.blockSize = 20;
+        this.xInitialBlock = 0;
+        this.yInitialBlock = 0.1;
+        this.zInitialBlock = (sideSize*this.blockSize)/2;
+        this.sideSize = sideSize;
+        this.type = type;
+        this.blocks = [new Block(this.xInitialBlock, this.yInitialBlock, this.zInitialBlock, true)];
+        this.xPos = this.xInitialBlock;
+        this.zPos = this.zInitialBlock;
+        this.cornersX = [];
+        this.cornersZ = [];
+        this.piecesCount = 0;
+        if(type == 1) this.createTrack1();
+        if(type == 2) this.createTrack2();
+    }
+
+    addBlock(x, y, z){
+        this.blocks.push(new Block(x, y, z, false));
+    }
+
+    createTrack1() 
+    {
+        for(var i= 1; i<this.sideSize/2; i++){
+            this.xPos -= this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+        console.log(this.cornersX[0]);
+
+        for(var i =1; i<this.sideSize; i++){
+            this.zPos -= this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+        
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =1; i<this.sideSize; i++){
+            this.xPos += this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =1; i<this.sideSize; i++){
+            this.zPos += this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =2; i<this.sideSize/2; i++){
+            this.xPos -= this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+        if(this.sideSize%2 == 0){
+            this.xPos -= this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+    }
+
+    createTrack2() 
+    {        
+        for(var i =1; i<this.sideSize/2; i++){
+            this.xPos -= this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+        
+        for(var i =1; i<this.sideSize; i++){
+            this.zPos -= this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =1; i<this.sideSize/2; i++){
+            this.xPos += this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =1; i<this.sideSize/2; i++){
+            this.zPos += this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =1; i<this.sideSize/2; i++){
+            this.xPos += this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =1; i<this.sideSize/2; i++){
+            this.zPos += this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+
+        this.cornersX.push(this.xPos);
+        this.cornersZ.push(this.zPos);
+
+        for(var i =2; i<this.sideSize/2; i++){
+            this.xPos -= this.blockSize;
+            this.addBlock(this.xPos, this.yInitialBlock, this.zPos, false);
+        }
+    }
+}
+
+export class LapInfo
+{
+  constructor(defaultText) {
+    this.box = document.createElement('div');
+    this.box.id = "box";
+    this.box.style.padding = "6px 14px";
+    this.box.style.top = "0";
+    this.box.style.left= "0";
+    this.box.style.position = "fixed";
+    this.box.style.backgroundColor = "rgba(100,100,255,0.3)";
+    this.box.style.color = "white";
+    this.box.style.fontFamily = "sans-serif";
+    this.box.style.fontSize = "16px";
+
+    this.stopwatch = document.createTextNode(defaultText);
+    this.box.appendChild(this.stopwatch);
+    this.addParagraph();
+    this.lap = document.createTextNode(defaultText);
+    this.box.appendChild(this.lap);
+    document.body.appendChild(this.box);
+  }
+  changeStopwatch(newText) {
+    this.stopwatch.nodeValue = newText;
+  }
+  changeLap(newText){
+    this.lap.nodeValue = newText;
+  }
+  
+  add(text) {
+    this.addParagraph();
+    var textnode = document.createTextNode(text);
+    this.box.appendChild(textnode);
+  }
+
+  addParagraph() {
+    const paragraph = document.createElement("br")
+    this.box.appendChild(paragraph);              ;
+  }
+}
+
+export class Stopwatch{
+    constructor(){
+        this.hh = 0;
+        this.mm = 0;
+        this.ss = 0;
+        this.ms = 0;
+        this.tempo = 10; //num de milésimos por ms
+        this.cron;
+        this.format = "00:00:00";
+    }
+
+    start() {
+        this.cron = setInterval(() => {this.timer();}, this.tempo);
+    }
+
+    pause() {
+        clearInterval(this.cron);
+    }
+
+    clear(){
+        this.hh = 0;
+        this.mm = 0;
+        this.ss = 0;
+        this.ms = 0;
+        this.format = (this.mm < 10 ? '0'+this.mm : this.mm) + ':' + (this.ss < 10 ? '0' + this.ss : this.ss) + ':' + (this.ms < 10 ? '0' + this.ms : this.ms);
+    }
+
+    stop() {
+        clearInterval(this.cron);
+        this.hh = 0;
+        this.mm = 0;
+        this.ss = 0;
+        this.ms = 0;
+        this.format = (this.mm < 10 ? '0'+this.mm : this.mm) + ':' + (this.ss < 10 ? '0' + this.ss : this.ss) + ':' + (this.ms < 10 ? '0' + this.ms : this.ms); 
+    }
+    timer(){
+
+        this.ms++;
+
+        if(this.ms == 100){
+            this.ms = 0;
+            this.ss++;
+
+            if(this.ss == 60){
+                this.ss = 0;
+                this.mm++;
+                if(this.mm == 60){
+                    this.mm = 0;
+                    this.hh++;
+                }
+            }
+        }
+
+        this.format = (this.mm < 10 ? '0'+this.mm : this.mm) + ':' + (this.ss < 10 ? '0' + this.ss : this.ss) + ':' + (this.ms < 10 ? '0' + this.ms : this.ms);
+    }
+}
+
+export class gameInfo{
+    constructor() {
+        this.infoBox = document.createElement('div');
+        this.infoBox.id = "InfoxBox";
+        this.infoBox.style.padding = "6px 14px";
+        this.infoBox.style.position = "fixed";
+        this.infoBox.style.bottom = "0";
+        this.infoBox.style.right = "0";
+        this.infoBox.style.backgroundColor = "rgba(255,255,255,0.7)";
+        this.infoBox.style.color = "white";
+        this.infoBox.style.fontFamily = "sans-serif";
+        this.infoBox.style.userSelect = "none";
+        this.infoBox.style.textAlign = "left";
+      }
+    
+      addParagraph() {
+        const paragraph = document.createElement("br")
+        this.infoBox.appendChild(paragraph);              ;
+      }
+    
+      add(text) {
+        var textnode = document.createTextNode(text);
+        this.infoBox.appendChild(textnode);
+        this.addParagraph();
+      }
+    
+      show() {
+        document.body.appendChild(this.infoBox);
+      }
+}
+
+/*export function finishLap(blocks) {
+    var finishedLap = true;
+    blocks.forEach(function(block){
+        if(block.passedBy == false)
+        finishedLap = false;
+    })
+    return finishedLap;
+}*/
+/*
+export function createTrack1(numBlocksPerSide) 
+{
+    var xPos = 0;
+    var yPos = - (numBlocksPerSide*10)/2;
+    var zPos = 0.1;
+    var track = [new Block(xPos, yPos, zPos, true)];
+
+    for(var i= 1; i<numBlocksPerSide/2; i++){
+        xPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =1; i<numBlocksPerSide; i++){
+        yPos += 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =1; i<numBlocksPerSide; i++){
+        xPos += 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =1; i<numBlocksPerSide; i++){
+        yPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =2; i<numBlocksPerSide/2; i++){
+        xPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+    if(numBlocksPerSide%2 == 0){
+        xPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    return track;
+}
+
+export function createTrack2(numBlocksPerSide) 
+{
+    var xPos = 0;
+    var yPos = - (numBlocksPerSide*10)/2;
+    var zPos = 0.1;
+    var track = [new Block(xPos, yPos, zPos, true)];
+    
+    for(var i =1; i<numBlocksPerSide/2; i++){
+        xPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+    
+    for(var i =1; i<numBlocksPerSide; i++){
+        yPos += 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =1; i<numBlocksPerSide/2; i++){
+        xPos += 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =1; i<numBlocksPerSide/2; i++){
+        yPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =1; i<numBlocksPerSide/2; i++){
+        xPos += 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =1; i<numBlocksPerSide/2; i++){
+        yPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    for(var i =2; i<numBlocksPerSide/2; i++){
+        xPos -= 10;
+        track.push(new Block(xPos, yPos, zPos, false));
+    }
+
+    return track;
+}*/
---
+++ works/game.js
@@ -0,0 +1,271 @@
+import * as THREE from  '../build/three.module.js';
+import Stats from       '../build/jsm/libs/stats.module.js';
+import {GUI} from       '../build/jsm/libs/dat.gui.module.js';
+import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import KeyboardState from '../libs/util/KeyboardState.js';
+import {initRenderer,
+        initDefaultBasicLight,
+        createGroundPlaneWired,
+      degreesToRadians} from "../libs/util/util.js";
+
+import {LapInfo, Stopwatch, Speedway, gameInfo} from './enviroment.js';
+import { Car} from './car.js';
+    
+
+var stats = new Stats();          // To show FPS information
+var scene = new THREE.Scene();    // Create main scene
+var renderer = initRenderer();    // View function in util/utils
+renderer.setClearColor("rgb(30, 30, 40)");
+
+//Stopwatch flags
+var startStopwatchFlag = true;
+var firstLapFlag = true;
+var secLapFlag = true;
+var thirdLapFlag = true;
+var fourthLapFlag = true;
+
+var gameIsOnFlag = true;
+
+var lapTimes = [];
+
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.lookAt(0, 0, 0);
+  camera.position.set(0,80,80);
+  camera.up.set( 0, 1, 0);
+var TrackballCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  TrackballCamera.lookAt(0, 0, 0);
+  TrackballCamera.position.set(0,80,80);
+  TrackballCamera.up.set( 0, 1, 0 );
+
+scene.add(camera);
+scene.add(TrackballCamera);
+
+var trackballControls = new TrackballControls( TrackballCamera, renderer.domElement );
+
+var cameraFree = false;
+
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+window.addEventListener( 'resize', function(){onWindowResize(TrackballCamera, renderer)}, false );
+
+// To use the keyboard
+var keyboard = new KeyboardState();
+
+// Show axes (parameter is size of each axis)
+//var axesHelper = new THREE.AxesHelper( 12 );
+//scene.add( axesHelper );
+
+//Light
+initDefaultBasicLight(scene, true);
+
+//Create the ground plane
+var plane = createGroundPlaneWired(600, 600, 50, 50); // width and height
+scene.add(plane);
+
+//Create Speedway
+var speedway = new Speedway(21, 2);
+speedway.blocks.forEach(function(block) {
+  scene.add(block.block); //Adiciona na cena cada cube do array de blocos 
+  scene.add(block.fundo); //Adiciona na cena o fundo de cada cube do array de blocos 
+})
+
+//Create car
+var car = new Car(1)
+scene.add(car.group);
+car.placeInitialPosition(speedway.sideSize);
+car.group.scale.set(0.3, 0.3, 0.3);
+car.updateNumCorners(speedway);
+
+camera.position.set(car.group.position.x +60 ,car.group.position.y + 60,60);
+
+//Create Stopwatches
+var stopwatch = new Stopwatch();
+var swLaps = new Stopwatch();
+
+
+//Move
+
+function keyboardUpdate() {
+
+  keyboard.update();
+
+  var direction = 0;
+
+  if ( keyboard.pressed("up")){
+    direction = 1;
+    car.accelerate(direction, speedway);
+
+    if(startStopwatchFlag){
+      stopwatch.start();
+      swLaps.start();
+      startStopwatchFlag = false;
+    }
+  }
+  if ( keyboard.pressed("down") )
+  {
+    direction = -2;
+    car.accelerate(direction, speedway);
+  }
+
+  var angle = degreesToRadians(3);
+  if ( keyboard.pressed(",") )  car.group.rotateY(  angle );
+  if ( keyboard.pressed(".") ) car.group.rotateY( -angle );
+
+  if ( keyboard.pressed("left") ){
+    car.goLeft(angle);
+  }else{
+    car.stop();
+  }
+  if ( keyboard.pressed("right") ){
+   car.goRight(angle);
+  }
+
+
+  if(direction == 0)  car.slowdown();
+  car.group.translateZ(car.velocity);
+
+}
+
+//Camera
+
+function cameraControl()
+{
+  changeCamera();
+  
+  if (cameraFree)
+  {
+    trackballControls.update();
+    trackballControls.target.set( car.group.position.x, car.group.position.y, car.group.position.z );
+  }
+  else
+    cameraFollow();
+}
+
+function changeCamera()
+{
+  if ( keyboard.down("space") )
+  {
+    // Troca de camera
+    cameraFree = !cameraFree;
+    setupCamera();
+  }
+}
+
+function setupCamera()
+{
+  
+  if (!cameraFree)
+    {
+      // Quando desativado o trackball
+      // Ativar outros objetos a serem visíveis
+      speedway.blocks.forEach(function(block){
+        block.cube.visible = true;
+        block.cubeFundo.visible = true;
+      })
+      plane.visible = true;
+    
+    }
+    else 
+    {
+      // Quando ativado o trackball
+      // Arrumar Posicao
+      trackballControls.reset;
+      trackballControls.target.set( car.group.position.x, car.group.position.y, car.group.position.z );
+      TrackballCamera.up.set( 0,1,0 );
+      TrackballCamera.position.set(car.group.position.x + 50, 50, car.group.position.z + 50);
+      TrackballCamera.lookAt(car.group.position);
+      
+      // Desativar visibilidade de outros objetos
+      speedway.blocks.forEach(function(block){
+        block.cube.visible = false;
+        block.cubeFundo.visible = false;
+      })
+
+      plane.visible = false;
+    }
+}
+
+function cameraFollow()
+{
+  var objectToFollow = car.group;
+  var dir = new THREE.Vector3();
+  car.group.getWorldDirection(dir);
+  camera.position.set(objectToFollow.position.x + dir.x*3 + 50, 50, objectToFollow.position.z + 50 + dir.z*3);
+  camera.lookAt(objectToFollow.position.x + dir.x*3, 0, objectToFollow.position.z +dir.z*3);
+}
+
+function cameraRenderer ()
+{
+  if (cameraFree)
+    renderer.render(scene, TrackballCamera);
+  else
+    renderer.render(scene, camera);
+}
+
+
+//Laps Info
+var stopwatchInfo = new LapInfo();
+function updateLapInfo() {
+  stopwatchInfo.changeStopwatch(stopwatch.format);
+  stopwatchInfo.changeLap("Lap: " + car.lap + "/4");
+
+  if((car.lap == 1) && firstLapFlag){
+    stopwatchInfo.add("Lap 1: " + swLaps.format);
+    lapTimes.push(swLaps.format);
+    swLaps.clear();
+    firstLapFlag = false;
+  }else {
+    if((car.lap == 2) && secLapFlag){
+      stopwatchInfo.add("Lap 2: " + swLaps.format);
+      lapTimes.push(swLaps.format);
+      swLaps.clear();
+      secLapFlag = false;
+    }else{
+      if((car.lap == 3) && thirdLapFlag){
+        stopwatchInfo.add("Lap 3: " + swLaps.format);
+        lapTimes.push(swLaps.format);
+        swLaps.clear();
+        thirdLapFlag = false;
+      }else{
+        if((car.lap == 4) && fourthLapFlag){
+          stopwatchInfo.add("Lap 4: " + swLaps.format);
+          lapTimes.push(swLaps.format);
+          swLaps.clear();
+          fourthLapFlag = false;
+        }
+      }
+    }
+  }
+}
+
+function isGameOver(){
+  if(car.lap == 4){
+    if(gameIsOnFlag)  gameOverInf();
+    gameIsOnFlag = false;
+
+  }
+}
+
+var gameOverInfo = new gameInfo();
+function gameOverInf(){
+  gameOverInfo.add("Total time : " + stopwatch.format);
+  stopwatch.stop();
+  for(var i=0; i < lapTimes.length; i++){
+    gameOverInfo.add((i+1) + "º lap: " + lapTimes[i]);
+  }
+  gameOverInfo.show();
+}
+
+render();
+function render()
+{
+  stats.update(); // Update FPS
+  if(gameIsOnFlag){
+    keyboardUpdate();
+    car.movement(speedway);
+  }
+  updateLapInfo();
+  isGameOver();
+  cameraControl();
+  requestAnimationFrame(render);
+  cameraRenderer(); // Render scene 
+}
